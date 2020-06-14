85f7fdfa9769c836a37b3b806ca63151339f06c3
Eldar <emmagomedov_95@inbox.ru>
2020-06-14 15:59:42 +0300

    Слайдер

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,5 @@
+// const { get } = require("browser-sync");
+
 function testWebP(callback) {
 
 	var webP = new Image();
@@ -16,3 +18,53 @@ testWebP(function (support) {
 		document.querySelector('body').classList.add('no-webp');
 	}
 });
+
+'use strict'
+
+const feedbackUser = document.querySelectorAll('.feedback__user'),
+	  btnNext = document.querySelector('.next'),
+	  btnPrev = document.querySelector('.prev'),
+	  slide = document.querySelector('.feedback__user'),
+	  feedbackDescr = document.querySelector('.feedback__descr');
+
+let counter = 1;
+
+const width = parseFloat(window.getComputedStyle(slide).width);
+let offset = 0;
+
+const showSlider = (n) => {
+	feedbackUser.forEach(item => {
+		item.style.cssText = `
+			transform: translateX(${offset}px) scale(0.6);
+			opacity: 0.5;
+			transition: all 0.3s linear;
+		`
+	})
+	feedbackUser[n].style.cssText = `
+		transform: translateX(${offset}px) scale(1);
+		opacity: 1;
+		transition: all 0.3s linear;
+	`
+}
+
+showSlider(1);
+
+btnNext.addEventListener('click', () => {
+	offset -= width;
+	counter++;
+	if (counter == (feedbackUser.length)) {
+		offset = width;
+		counter = 0;
+	}
+	showSlider(counter);
+})
+
+btnPrev.addEventListener('click', () => {
+	offset += width;
+	counter--;
+	if (counter < 0) {
+		offset = -width * (feedbackUser.length - 2);
+		counter = feedbackUser.length - 1
+	}
+	showSlider(counter);
+})
