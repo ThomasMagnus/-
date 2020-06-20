111bff6bbd7bd6cca93e8c5a23447124710550b4
Eldar <emmagomedov_95@inbox.ru>
2020-06-21 00:48:10 +0300

    Верстка готова

--- dist/js/script.js
+++ dist/js/script.js
@@ -19,52 +19,57 @@ testWebP(function (support) {
 	}
 });
 
-'use strict'
+window.addEventListener('DOMContentLoaded', () => {
+	'use strict'
 
-const feedbackUser = document.querySelectorAll('.feedback__user'),
-	  btnNext = document.querySelector('.next'),
-	  btnPrev = document.querySelector('.prev'),
-	  slide = document.querySelector('.feedback__user'),
-	  feedbackDescr = document.querySelector('.feedback__descr');
-
-let counter = 1;
-
-const width = parseFloat(window.getComputedStyle(slide).width);
-let offset = 0;
-
-const showSlider = (n) => {
-	feedbackUser.forEach(item => {
-		item.style.cssText = `
-			transform: translateX(${offset}px) scale(0.6);
-			opacity: 0.5;
+	// Слайдер
+	
+	const feedbackUser = document.querySelectorAll('.feedback__user'),
+		  btnNext = document.querySelector('.next'),
+		  btnPrev = document.querySelector('.prev'),
+		  slide = document.querySelector('.feedback__user'),
+		  feedbackDescr = document.querySelector('.feedback__descr');
+	
+	let counter = 1;
+	
+	const width = parseFloat(window.getComputedStyle(slide).width);
+	let offset = 0;
+	
+	const showSlider = (n) => {
+		feedbackUser.forEach(item => {
+			item.style.cssText = `
+				transform: translateX(${offset}px) scale(0.6);
+				opacity: 0.5;
+				transition: all 0.3s linear;
+			`
+		})
+		feedbackUser[n].style.cssText = `
+			transform: translateX(${offset}px) scale(1);
+			opacity: 1;
 			transition: all 0.3s linear;
 		`
+	}
+	
+	showSlider(1);
+	
+	btnNext.addEventListener('click', () => {
+		offset -= width;
+		counter++;
+		if (counter == (feedbackUser.length)) {
+			offset = width;
+			counter = 0;
+		}
+		showSlider(counter);
 	})
-	feedbackUser[n].style.cssText = `
-		transform: translateX(${offset}px) scale(1);
-		opacity: 1;
-		transition: all 0.3s linear;
-	`
-}
-
-showSlider(1);
-
-btnNext.addEventListener('click', () => {
-	offset -= width;
-	counter++;
-	if (counter == (feedbackUser.length)) {
-		offset = width;
-		counter = 0;
-	}
-	showSlider(counter);
+	
+	btnPrev.addEventListener('click', () => {
+		offset += width;
+		counter--;
+		if (counter < 0) {
+			offset = -width * (feedbackUser.length - 2);
+			counter = feedbackUser.length - 1
+		}
+		showSlider(counter);
+	})
 })
 
-btnPrev.addEventListener('click', () => {
-	offset += width;
-	counter--;
-	if (counter < 0) {
-		offset = -width * (feedbackUser.length - 2);
-		counter = feedbackUser.length - 1
-	}
-	showSlider(counter);
-})
